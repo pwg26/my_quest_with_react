d07a4f6bc9b0ce784f1bc1b5ab1ead72a983c5c9
Pwg21 <kcaskade@gmail.com>
2021-03-24 17:27:50 -0600

    finished footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,10 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Wikipedia Searcher</span>
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <span className="text-dark">Designed in Denver Colorado </span>
+      </div>
     </footer>
   );
 }
