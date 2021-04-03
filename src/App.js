cf3e2ac34431353b13308d36d592afa4d1f72c9b
Pierce Gladfelter <kcaskade@gmail.com>
2021-04-03 00:07:51 -0400

    Update App.js
    changed hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 
-import React from "react";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
